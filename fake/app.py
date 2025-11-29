4d9cc2aaf0ccde38241ec4bc913b0def24a2623e
ZahidHasan000 <h.zahid007.zh59@gmail.com>
2025-11-30 01:20:35 +0600

    Backend for deploy

--- fake/app.py
+++ fake/app.py
@@ -5,7 +5,8 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 app.secret_key = "secret key"
 # app.config["MONGO_URI"] = "mongodb+srv://ipsita:Ipsita%402023@uk-bd0.u3pngqk.mongodb.net/airbnb"
-app.config["MONGO_URI"] = "mongodb://localhost:27017/property_pricing"
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/property_pricing"
+app.config["MONGO_URI"] = "mongodb+srv://zahidhasan:zahid3060@cluster0.3ozgvyw.mongodb.net/Lexical?retryWrites=true&w=majority"
 app.config["CORS_HEADERS"] = "Content-Type"
 mongo = PyMongo(app)
 
